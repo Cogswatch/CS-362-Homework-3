55ab419ce03d23f6bbaa676156b947596661db0e
C. Greyston Brady <c.greyston.brady@gmail.com>
2021-05-04 23:38:41 -0700

    Forgot to save first smh

--- Question1.py
+++ Question1.py
@@ -1,1 +1,9 @@
 import unittest
+
+# Write code to calculate the volume of a cube
+def cube(sideLength):
+  return sideLength * sideLength * sideLength
+
+assert cube(10) == 1000
+assert cube(0) == 0
+assert cube(-10) == -1000
